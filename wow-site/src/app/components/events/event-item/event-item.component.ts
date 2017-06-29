ed22402e7c0e1f15262bd8179287b72bedc86292
ner <inerono@gmail.com>
2017-06-29 17:53:57 +0300

    IE fixes.

--- wow-site/src/app/components/events/event-item/event-item.component.ts
+++ wow-site/src/app/components/events/event-item/event-item.component.ts
@@ -1,5 +1,6 @@
-import {Component, Input, EventEmitter, Output} from '@angular/core';
-import {Event} from "../../../entities/event";
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { Event } from "../../../entities/event";
+import { WindowRef } from "app/services/window.service";
 
 declare const UIkit: any;
 
@@ -14,6 +15,8 @@ export class EventItemComponent {
   @Input() currentEvent: Event;
   @Output() onCurrentEventChange: EventEmitter<Event> = new EventEmitter();
 
+  constructor(private _window: WindowRef) { }
+
   openEventDetails(event: Event) {
     this.currentEvent = event;
     this.onCurrentEventChange.emit(event);
@@ -24,6 +27,24 @@ export class EventItemComponent {
     this.currentEvent = event;
     this.onCurrentEventChange.emit(event);
     UIkit.modal('#subscriptionModal').show();
+  }
+
+  isIE() {
+    const ua = window.navigator.userAgent;
+    const msie = ua.indexOf('MSIE ');
+    if (msie > 0) {
+      return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+    }
+    const trident = ua.indexOf('Trident/');
+    if (trident > 0) {
+      const rv = ua.indexOf('rv:');
+      return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+    }
+    const edge = ua.indexOf('Edge/');
+    if (edge > 0) {
+      return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+    }
+    return false;
   }
 
 }
